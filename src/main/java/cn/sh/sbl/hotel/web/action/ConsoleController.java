ba464c753d5e1058ceb8740775c06d9a6c866999
Bunco <amwihc512@qq.com>
2013-11-24 11:17:50 +0800

    Add a file upload support and examples

--- src/main/java/cn/sh/sbl/hotel/web/action/ConsoleController.java
+++ src/main/java/cn/sh/sbl/hotel/web/action/ConsoleController.java
@@ -7,20 +7,25 @@
  */
 package cn.sh.sbl.hotel.web.action;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import cn.sh.sbl.hotel.beans.Actor;
-import cn.sh.sbl.hotel.beans.Film;
 import cn.sh.sbl.hotel.service.IActorService;
 import cn.sh.sbl.hotel.service.IFilmService;
 import cn.sh.sbl.hotel.service.IMenuService;
-import cn.sh.sbl.hotel.vo.ActorVo;
 
 /**
  * @author samsung 
@@ -29,11 +34,8 @@ import cn.sh.sbl.hotel.vo.ActorVo;
  * @date 2013-11-23 下午7:27:07
  * @description TODO
  */
-
 @Controller
 public class ConsoleController {
-	
-	
 	@Autowired
 	private Logger logger;
 	@Autowired
@@ -49,18 +51,37 @@ public class ConsoleController {
 	 * @param modelMap
 	 * @return
 	 */
-	@RequestMapping(value={"/Consoletest"})
+	@RequestMapping(value={"/upload/{name}"}, method=RequestMethod.POST)
 	@Transactional
-	public ModelAndView demo(ModelMap modelMap) {
-		Actor actor = this.actorService.get(1);
-		ActorVo actorVo = new ActorVo();
-		for (Film film : actor.getFilms()) {
-			actorVo.setFirstName(film.getTitle());
-			actorVo.setLastName(String.valueOf(film.getDirectors().size()));
-			actorVo.setId(actor.getId());
+	public ModelAndView demo(@PathVariable("name")String name, @RequestParam("file")MultipartFile[] files,
+			HttpServletRequest request, ModelMap modelMap) {
+		String realPath = request.getSession().getServletContext().getRealPath("/upload");  
+		logger.debug("start uplpad file! upload path: {}", realPath);
+		boolean success = true;
+		if (0 < files.length) {
+			for (MultipartFile multipartFile : files) {
+				if (multipartFile.isEmpty()) {
+					logger.debug("empty file: {}!", multipartFile);
+					continue;
+				}
+				logger.debug("file original name: {}, name: {}, size: {}, type: {}", 
+						multipartFile.getOriginalFilename(), multipartFile.getName(),
+						multipartFile.getSize(), multipartFile.getContentType());
+				try {
+					multipartFile.transferTo(new File(realPath + "/" + multipartFile.getOriginalFilename()));
+					modelMap.put(multipartFile.getOriginalFilename(), "upload successfull!");
+				} catch (Exception e) {
+					success = false;
+					logger.error("", e);
+					modelMap.put(multipartFile.getOriginalFilename(), e.getMessage());
+				}
+			}
+			modelMap.put("result", success);
+		} else {
+			modelMap.put("result", false);
+			modelMap.put("errMsg", "Please select a file to upload!");
 		}
-		modelMap.put("Actor", actorVo);
-		return new ModelAndView("hello", modelMap);
+		return new ModelAndView("upload", modelMap);
 	}
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,57 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-</body>
-</html>
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>  
+<%  
+String path = request.getContextPath();  
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";  
+%>  
+  
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">  
+<html>  
+  <head>  
+    <base href="<%=basePath%>">  
+    <script type="text/javascript" src="<%=request.getContextPath()%>/js/jquery-1.10.2.min.js"></script>  
+<script type="text/javascript" src="<%=request.getContextPath()%>/js/ajaxfileupload.js"></script>  
+  
+<script type="text/javascript">  
+function ajaxFileUpload(){  
+    //开始上传文件时显示一个图片,文件上传完成将图片隐藏  
+    //$("#loading").ajaxStart(function(){$(this).show();}).ajaxComplete(function(){$(this).hide();});  
+    //执行上传文件操作的函数  
+    $.ajaxFileUpload({  
+        //处理文件上传操作的服务器端地址(可以传参数,已亲测可用)  
+        url:'<%=request.getContextPath()%>/upload/sldf.json',  
+        secureuri:false,                       //是否启用安全提交,默认为false   
+        fileElementId:'myBlogImage',           //文件选择框的id属性  
+        dataType:'json',                       //服务器返回的格式,可以是json或xml等  
+        success:function(data, status){        //服务器响应成功时的处理函数  
+//             data = data.replace("<PRE>", '');  //ajaxFileUpload会对服务器响应回来的text内容加上<pre>text</pre>前后缀  
+//             data = data.replace("</PRE>", '');  
+//             data = data.replace("<pre>", '');  
+//             data = data.replace("</pre>", ''); //本例中设定上传文件完毕后,服务端会返回给前台[0`filepath]  
+//             if(data.substring(0, 1) == 0){     //0表示上传成功(后跟上传后的文件路径),1表示失败(后跟失败描述)  
+//                 $("img[id='uploadImage']").attr("src", data.substring(2));  
+//                 $('#result').html("图片上传成功<br/>");  
+//             }else{  
+//                 $('#result').html('图片上传失败，请重试！！');  
+//             }  
+			alert("图片上传成功: " + data);
+        },  
+        error:function(data, status, e){ //服务器响应失败时的处理函数  
+//            $('#result').html('图片上传失败，请重试！！');  
+            alert('图片上传失败，请重试！！');  
+        }  
+    });  
+}  
+</script> 
+  </head>  
+  <body>  
+     <form id="addUserInfoForm" action="<%=request.getContextPath()%>/upload/asldf.json" method="post" enctype="multipart/form-data">  
+<%--     <input name="userName"  value="${user.userName}"/>   --%>
+<%--       <input name="userPwd" value="${user.userPwd}"/>   --%>
+<!--       <input type="file" name="file">   -->
+      <input id="myBlogImage" type="file" name="file"> <br/> 
+      <input type="submit"  value="提交"/>  
+      <input type="button" value="Ajax提交" onclick="ajaxFileUpload()"/>  
+      
+    </form>   
+  </body>  
+</html>  
